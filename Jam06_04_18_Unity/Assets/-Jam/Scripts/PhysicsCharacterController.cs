3ca44c294affc9ecfbd45044c2c31a7530c065b7
Kieran Lord <cratesmith@cratesmith.com>
2018-04-07 10:57:32 +1000

    moved physics character settings into an asset

--- Jam06_04_18_Unity/Assets/-Jam/Scripts/PhysicsCharacterController.cs
+++ Jam06_04_18_Unity/Assets/-Jam/Scripts/PhysicsCharacterController.cs
@@ -3,65 +3,13 @@ using UnityEngine;
 
 public class PhysicsCharacterController : SubComponent<Actor>
 {
-    [SerializeField] private float m_onGroundVelocityBlend = 25;
-    [SerializeField] private float m_groundBreakVelocityBlend = 30;
-    [SerializeField] private float m_airVelocityBlend = 5;
-    [SerializeField] private float m_jumpImpulse = 10;
-    [SerializeField] private float m_gravityFactor = 2;
-    [SerializeField] private float m_maxSlopeAngle = 45;
-    [SerializeField] private float m_maxSpeed = 10;
+    public PhysicsCharacterControllerSettings.Reference settings = new PhysicsCharacterControllerSettings.Reference();
 
     public bool isGrounded { get; private set; }
     public bool jump { get; set; }
     public Vector2 moveXZ { get; set; }
 
     private bool m_isJumpRising;
-
-    [System.Serializable]
-    public class ColliderSettings
-    {
-        public float height;
-        public float radius;
-        public float stepHeight = .25f;
-
-        private static PhysicMaterial s_physMaterial;
-
-        public void Apply(CapsuleCollider capsuleCollider)
-        {
-            capsuleCollider.radius = radius;
-            capsuleCollider.height = height - stepHeight;
-            capsuleCollider.center = new Vector3(0, capsuleCollider.height / 2f + stepHeight, 0);
-            if (s_physMaterial == null)
-            {
-                s_physMaterial = new PhysicMaterial("PhysicsCharacter");
-                s_physMaterial.dynamicFriction = 0;
-                s_physMaterial.staticFriction = 0;
-                s_physMaterial.bounceCombine = PhysicMaterialCombine.Minimum;
-                s_physMaterial.frictionCombine = PhysicMaterialCombine.Minimum;
-                s_physMaterial.bounciness = 0;
-            }
-            capsuleCollider.sharedMaterial = s_physMaterial;
-        }
-    }
-
-    [SerializeField] ColliderSettings m_colliderSettings = new ColliderSettings();
-
-
-    [System.Serializable]
-    public class RigidbodySettings
-    {
-        public float mass = 1f;
-
-        public void Apply(Rigidbody rigidBody)
-        {
-            rigidBody.mass = mass;
-            rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
-            rigidBody.useGravity = false;
-        }
-    }
-
-    [SerializeField] private RigidbodySettings m_rigidbodySettings = new RigidbodySettings();
-
     private Vector3 m_prevPosition;
     private Rigidbody m_rigidBody;
     private CapsuleCollider m_capsuleCollider;
@@ -72,10 +20,10 @@ public class PhysicsCharacterController : SubComponent<Actor>
     protected void Awake()
     {
         m_rigidBody = gameObject.GetOrAddComponent<Rigidbody>();
-        m_rigidbodySettings.Apply(m_rigidBody);
+        settings.value.rigidbodySettings.Apply(m_rigidBody);
 
         m_capsuleCollider = gameObject.GetOrAddComponent<CapsuleCollider>();
-        m_colliderSettings.Apply(m_capsuleCollider);
+        settings.value.colliderSettings.Apply(m_capsuleCollider);
     }
 
     void Start()
@@ -99,28 +47,29 @@ public class PhysicsCharacterController : SubComponent<Actor>
 
     private void FixedUpdate_Move()
     {
-        var desiredVelocity = m_maxSpeed * moveXZ.X_Y() + Vector3.up * m_rigidBody.velocity.y;
+        var settings = this.settings.value;
+        var desiredVelocity = settings.maxSpeed * moveXZ.X_Y() + Vector3.up * m_rigidBody.velocity.y;
 
         // check if the jump is still rising
         m_isJumpRising &= m_rigidBody.velocity.y > 0;
 
         // stepRay - find the ground
-        var localStepRayOrigin = new Vector3(0, m_colliderSettings.stepHeight, 0);
+        var localStepRayOrigin = new Vector3(0, settings.colliderSettings.stepHeight, 0);
         var mainStepRay = new Ray(transform.TransformPoint(localStepRayOrigin), -transform.up);
         var stepRay = mainStepRay;
         RaycastHit raycastHit;
-        var mainStepRayResult = Physics.Raycast(stepRay, out raycastHit, m_colliderSettings.stepHeight * 2);
+        var mainStepRayResult = Physics.Raycast(stepRay, out raycastHit, settings.colliderSettings.stepHeight * 2);
         if (!mainStepRayResult)
         {
-            if (Physics.SphereCast(transform.TransformPoint(new Vector3(0, m_colliderSettings.height, 0)),
-                m_colliderSettings.radius / 2f,
+            if (Physics.SphereCast(transform.TransformPoint(new Vector3(0, settings.colliderSettings.height, 0)),
+                settings.colliderSettings.radius / 2f,
                 -transform.up,
                 out raycastHit,
-                m_colliderSettings.height))
+                settings.colliderSettings.height))
             {
                 var localHit = transform.InverseTransformPoint(raycastHit.point);
                 stepRay.origin = transform.TransformPoint(new Vector3(localHit.x, localStepRayOrigin.y, localHit.z));
-                mainStepRayResult = Physics.Raycast(stepRay, out raycastHit, m_colliderSettings.stepHeight * 2);
+                mainStepRayResult = Physics.Raycast(stepRay, out raycastHit, settings.colliderSettings.stepHeight * 2);
             }
 
         }
@@ -128,9 +77,9 @@ public class PhysicsCharacterController : SubComponent<Actor>
         // if we found a ground. these are the variables for it
         var hitPointLocal = transform.InverseTransformPoint(raycastHit.point);
         var groundPoint = transform.TransformPoint(0, hitPointLocal.y, 0f);
-        var withinStepHeight = raycastHit.distance - 0.001f <= m_colliderSettings.stepHeight;
+        var withinStepHeight = raycastHit.distance - 0.001f <= settings.colliderSettings.stepHeight;
         var slopeAngle = Vector3.Angle(Vector3.up, raycastHit.normal);
-        var walkableSlope = slopeAngle < m_maxSlopeAngle;
+        var walkableSlope = slopeAngle < settings.maxSlopeAngle;
         var flatNormal = new Vector3(raycastHit.normal.x, 0, raycastHit.normal.z).normalized;
 
         // are we on the ground?
@@ -147,12 +96,12 @@ public class PhysicsCharacterController : SubComponent<Actor>
             isGrounded = false;
         }
 
-        Debug.DrawRay(stepRay.origin, stepRay.direction * m_colliderSettings.stepHeight * 2f,
+        Debug.DrawRay(stepRay.origin, stepRay.direction * settings.colliderSettings.stepHeight * 2f,
             raycastHit.collider != null ? Color.green : Color.red);
        
         // apply desired vel       
         var desiredVelDotVelXZ = Vector2.Dot(m_rigidBody.velocity.XZ(), moveXZ);
-        var blendRate = isGrounded ? (desiredVelDotVelXZ > 0 ? m_onGroundVelocityBlend : m_groundBreakVelocityBlend) : m_airVelocityBlend;
+        var blendRate = isGrounded ? (desiredVelDotVelXZ > 0 ? settings.onGroundVelocityBlend : settings.groundBreakVelocityBlend) : settings.airVelocityBlend;
         var desiredDotFlatNormal = Vector3.Dot(flatNormal, desiredVelocity);
         if (!walkableSlope)
         {
@@ -165,7 +114,7 @@ public class PhysicsCharacterController : SubComponent<Actor>
         m_rigidBody.velocity = Vector3.Lerp(m_rigidBody.velocity, desiredVelocity, Time.fixedDeltaTime * blendRate);
 
         // gravity
-        m_rigidBody.velocity += Physics.gravity * m_gravityFactor * Time.fixedDeltaTime;
+        m_rigidBody.velocity += Physics.gravity * settings.gravityFactor * Time.fixedDeltaTime;
 
         // stick us to the ground. modify our velocity if groundray has run into the ground
         if (isGrounded)
@@ -182,7 +131,7 @@ public class PhysicsCharacterController : SubComponent<Actor>
         // handle jump if we're still grounded
         if (jump && isGrounded)
         {            
-            m_rigidBody.velocity += Vector3.Lerp(m_groundNormal,transform.up,.5f) * m_jumpImpulse;
+            m_rigidBody.velocity += Vector3.Lerp(m_groundNormal,transform.up,.5f) * settings.jumpImpulse;
             jump = false;
             m_isJumpRising = true;
             isGrounded = false;
---
+++ Jam06_04_18_Unity/Assets/-Jam/Scripts/PhysicsCharacterControllerSettings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+public class PhysicsCharacterControllerSettings : SettingsAsset<PhysicsCharacterControllerSettings>
+{
+    public float onGroundVelocityBlend = 25;
+    public float groundBreakVelocityBlend = 30;
+    public float airVelocityBlend = 5;
+    public float jumpImpulse = 10;
+    public float gravityFactor = 2;
+    public float maxSlopeAngle = 45;
+    public float maxSpeed = 10;
+
+    [System.Serializable]
+    public class ColliderSettings
+    {
+        public float height;
+        public float radius;
+        public float stepHeight = .25f;
+
+        private static PhysicMaterial s_physMaterial;
+
+        public void Apply(CapsuleCollider capsuleCollider)
+        {
+            capsuleCollider.radius = radius;
+            capsuleCollider.height = height - stepHeight;
+            capsuleCollider.center = new Vector3(0, capsuleCollider.height / 2f + stepHeight, 0);
+            if (s_physMaterial == null)
+            {
+                s_physMaterial = new PhysicMaterial("PhysicsCharacter");
+                s_physMaterial.dynamicFriction = 0;
+                s_physMaterial.staticFriction = 0;
+                s_physMaterial.bounceCombine = PhysicMaterialCombine.Minimum;
+                s_physMaterial.frictionCombine = PhysicMaterialCombine.Minimum;
+                s_physMaterial.bounciness = 0;
+            }
+            capsuleCollider.sharedMaterial = s_physMaterial;
+        }
+    }
+
+    public ColliderSettings colliderSettings = new ColliderSettings();
+
+    [System.Serializable]
+    public class RigidbodySettings
+    {
+        public float mass = 1f;
+
+        public void Apply(Rigidbody rigidBody)
+        {
+            rigidBody.mass = mass;
+            rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+            rigidBody.useGravity = false;
+        }
+    }
+    public RigidbodySettings rigidbodySettings = new RigidbodySettings();
+
+    [Serializable]
+    public class Reference : SettingsReference<PhysicsCharacterControllerSettings>
+    {
+    }
+}
